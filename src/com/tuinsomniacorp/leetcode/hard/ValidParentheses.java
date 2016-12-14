92850b863597b681c1fb9e0d43fb048e015374f8
angeldeb <angeldeb@ANGELDEB01.corp.amdocs.com>
2016-12-13 19:06:22 -0600

    LeetCode Algorithms problem solving
    exercise: 20 Valid Parentheses 
    level: easy

---
+++ src/com/tuinsomniacorp/leetcode/hard/ValidParentheses.java
@@ -0,0 +1,59 @@
+package com.tuinsomniacorp.leetcode.hard;
+
+import java.util.Stack;
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ * @author Angel Banuelos ing.angel.banuelos@gmail.com
+ *
+ */
+public class ValidParentheses {
+
+	public boolean isValidParentheses(String s) {
+		
+		Stack<Character> stackIn = new Stack<>();
+		char[] chr = s.toCharArray();
+		char expected = '\u0000';
+		for (char c : chr) {
+			switch (c) {
+			case '(':
+			case '[':
+			case '{':
+				stackIn.push(c);
+				break;
+			case ')':
+				if (stackIn.isEmpty()) {
+					return false;
+				}
+				expected = '(';
+				char in = stackIn.pop();
+				if (in != expected) {
+					return false;
+				}
+				break;
+			case ']':
+				if (stackIn.isEmpty()) {
+					return false;
+				}
+				expected = '[';
+				in = stackIn.pop();
+				if (in != expected) {
+					return false;
+				}
+				break;
+			case '}':
+				if (stackIn.isEmpty()) {
+					return false;
+				}
+				expected = '{';
+				in = stackIn.pop();
+				if (in != expected) {
+					return false;
+				}
+				break;
+			}
+		}
+		return stackIn.isEmpty();
+	}
+			
+}
