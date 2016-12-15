74e90e0260e82c1ef2cd16d835b6e94c0b4fb949
angeldeb <angeldeb@ANGELDEB01.corp.amdocs.com>
2016-12-15 12:50:32 -0600

    PowN leetcode
    leetcode
    Num: 50. Pow(x, n) level: Medium

--- src/com/tuinsomniacorp/leetcode/medium/PowX.java
+++ src/com/tuinsomniacorp/leetcode/medium/PowX.java
@@ -1,7 +1,8 @@
 package com.tuinsomniacorp.leetcode.medium;
 
 /**
- * 
+ * 50. Pow(x, n)
+ * Implement pow(x, n).
  * 
  * @author Angel Banuelos ing.angel.banuelos@gmail.com
  *
@@ -9,6 +10,7 @@ package com.tuinsomniacorp.leetcode.medium;
 public class PowX {
 
 	public double myPow(double x, int n) {
+		
 		double pow = 1;
 		if (n % 2 == 0) {
 			double pow1 = pow(x, n / 2, pow);
@@ -20,9 +22,11 @@ public class PowX {
 			}
 			return pow1 * pow1 * x;
 		}
+		
 	}
 
 	public double myPowRec(double x, int n, double pow) {
+		
 		if(n == 0) {
 			return pow;
 		}
@@ -36,55 +40,70 @@ public class PowX {
 			return myPowRec(x, n, pow);
 		}
 		return pow;
+		
 	}
 
 	public double pow(double x, int n, double pow) {
-		if (pow == 0) {
-			return 0;
-		}
+		
 		if(n == 0 || x == 1) {
 			return pow;
 		}
+		if(n > Integer.MAX_VALUE || n < Integer.MIN_VALUE + 1){
+			return 0.0;
+		}
+		if (x == -1) {
+			if (n % 2 == 0 ) {
+				return 1;
+			} else {
+				return -1;
+			}
+		}
 		if (n > 0) {
-			for (int i = 0; i < n; i++) {
+			while(pow != 0 && n > 0 ){
+				n--;
 				pow *= x;
 			}
 		} else if (n < 0) {
-			for (int i = n; i < 0; i++) {
+			while(pow != 0 && n < 0 ){
+				n++;
 				pow /= x;
 			}
 		}
 		return pow;
+		
 	}
 	
+	/**
+	 * x^15
+	 *  		x^1				x
+	 * 			x^2			x		x
+	 * 			x^4		x		xx		x
+	 * 			x^8	x	x	x   xx x  x    x
+	 * x^15 = x^1 * x^2 * x^4 * x^8
+	 * if n = 15 then  binary number of 15 equal to  1 1 1 1
+	 * 												 8+4+2+1 = 15
+	 * @param x
+	 * @param n
+	 * @return pow 
+	 */
 	public double myPowBit(double x, int n) {
-		int binary = (n < 0 ? n * -1 : n);
+		int binary = (n < 0 ? -n : n);
 		int n1 = 0;
 		double pow = 1;
 		while (binary > 0) {
 			int bit = binary % 2;
 			int newN = bit * (1 << n1++);
 			binary /= 2;
+			if (newN == 0) {
+				continue;
+			}
 			if (n < 0) {
-				pow = pow(x, newN * (-1), pow);
+				pow = pow(x, -newN, pow);
+			}else {
 				pow = pow(x, newN, pow);
 			}
 		}
 		return pow;
-	}
-
-	public static void main(String[] args) {
-		PowX obj = new PowX();
-		long start = System.currentTimeMillis();
-		System.out.println("2 = " + (1 << -2147483648));
-		System.out.println(obj.myPowBit(2, -2147483648));
-		long end = System.currentTimeMillis();
-		System.out.println("Time: " + (end - start));
-		//1.00000
-//		2147483647
-		//2.00000
-//		-2147483648
-		
 	}
 
 }
