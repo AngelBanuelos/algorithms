2c5e24d767b93d941d93d13de7d90b7e2c0130ec
angeldeb <angeldeb@ANGELDEB01.corp.amdocs.com>
2016-12-16 17:23:26 -0600

    Fast implementation for PowX

--- src/com/tuinsomniacorp/learning/Algorithms.java
+++ src/com/tuinsomniacorp/learning/Algorithms.java
@@ -4,6 +4,17 @@ public class Algorithms {
 	
 	public static void main(String[] args) {
 		
+		
+		System.out.println((7&1) + " " + (7&2) + " " + (7&4) + " "  + (7&8) + " " + (7&16));
+		
+		System.out.println((7>>1&1) + " " + (7>>1&2) + " " + (7>>1&4) + " "  + (7>>1&8) + " " + (7>>1&16) + "\n" + (7>>1));
+		
+		System.out.println((7<<2));
+		
+		
+		System.out.println(" " + (24&1));
+		
+		
 	}
 
 }
--- src/com/tuinsomniacorp/leetcode/medium/PowX.java
+++ src/com/tuinsomniacorp/leetcode/medium/PowX.java
@@ -1,8 +1,7 @@
 package com.tuinsomniacorp.leetcode.medium;
 
 /**
- * 50. Pow(x, n)
- * Implement pow(x, n).
+ * 50. Pow(x, n) Implement pow(x, n).
  * 
  * @author Angel Banuelos ing.angel.banuelos@gmail.com
  *
@@ -10,7 +9,7 @@ package com.tuinsomniacorp.leetcode.medium;
 public class PowX {
 
 	public double myPow(double x, int n) {
-		
+
 		double pow = 1;
 		if (n % 2 == 0) {
 			double pow1 = pow(x, n / 2, pow);
@@ -22,12 +21,12 @@ public class PowX {
 			}
 			return pow1 * pow1 * x;
 		}
-		
+
 	}
 
 	public double myPowRec(double x, int n, double pow) {
-		
-		if(n == 0) {
+
+		if (n == 0) {
 			return pow;
 		}
 		if (n > 0) {
@@ -40,51 +39,46 @@ public class PowX {
 			return myPowRec(x, n, pow);
 		}
 		return pow;
-		
+
 	}
 
 	public double pow(double x, int n, double pow) {
-		
-		if(n == 0 || x == 1) {
+
+		if (n == 0 || x == 1) {
 			return pow;
 		}
-		if(n > Integer.MAX_VALUE || n < Integer.MIN_VALUE + 1){
+		if (n > Integer.MAX_VALUE || n < Integer.MIN_VALUE + 1) {
 			return 0.0;
 		}
 		if (x == -1) {
-			if (n % 2 == 0 ) {
+			if (n % 2 == 0) {
 				return 1;
 			} else {
 				return -1;
 			}
 		}
 		if (n > 0) {
-			while(pow != 0 && n > 0 ){
+			while (pow != 0 && n > 0) {
 				n--;
 				pow *= x;
 			}
 		} else if (n < 0) {
-			while(pow != 0 && n < 0 ){
+			while (pow != 0 && n < 0) {
 				n++;
 				pow /= x;
 			}
 		}
 		return pow;
-		
+
 	}
-	
+
 	/**
-	 * x^15
-	 *  		x^1				x
-	 * 			x^2			x		x
-	 * 			x^4		x		xx		x
-	 * 			x^8	x	x	x   xx x  x    x
-	 * x^15 = x^1 * x^2 * x^4 * x^8
-	 * if n = 15 then  binary number of 15 equal to  1 1 1 1
-	 * 												 8+4+2+1 = 15
+	 * x^15 x^1 x x^2 x x x^4 x xx x x^8 x x x xx x x x x^15 = x^1 * x^2 * x^4 *
+	 * x^8 if n = 15 then binary number of 15 equal to 1 1 1 1 8+4+2+1 = 15
+	 * 
 	 * @param x
 	 * @param n
-	 * @return pow 
+	 * @return pow
 	 */
 	public double myPowBit(double x, int n) {
 		int binary = (n < 0 ? -n : n);
@@ -99,11 +93,52 @@ public class PowX {
 			}
 			if (n < 0) {
 				pow = pow(x, -newN, pow);
-			}else {
+			} else {
 				pow = pow(x, newN, pow);
 			}
 		}
 		return pow;
+	}
+
+	public static double pow(double base, int exp) {
+
+		if(Integer.MIN_VALUE == exp) {
+			return 0; 
+		}
+		if(base == 1) {
+			return 1;
+		}
+		double result = 1;
+		boolean negative = (exp < 0 ? true : false);
+		
+		//if exp is negative change it to positive to use the bit pow  
+		if (negative) {
+			exp = -exp;
+		}
+
+		while (exp != 0) {
+			
+			//validate if the exp is pair or not
+			if ((exp & 1) == 1) {
+				if (!negative) {
+					result *= base;
+				} else {
+					result *= (1 / base);
+				}
+			}
+			exp >>= 1;
+			if (!negative) {
+				base *= base;
+			} else {
+				base /= (1 / base);
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(pow(-1, Integer.MAX_VALUE));
 	}
 
 }
